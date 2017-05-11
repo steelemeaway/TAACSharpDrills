141e529ec6a1f4cd5267258e0d03d8f7b4bce22a
Matt Steele <matt.tyler.steele@gmail.com>
2017-05-11 16:41:14 -0700

    First DevU challenge
    Built simple program

---
+++ FirstChallenge/FirstChallenge/Default.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FirstChallenge
+{
+    public partial class Default1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            string age = ageTextBox.Text;
+            string money = moneyTextBox.Text;
+
+            string result = "Thanks for soliciting my opinion. You rock my socks. Who cares if you're " + age + " years old and only have " + money + " monies! <3";
+
+            resultLabel.Text = result;
+
+        }
+    }
+}
